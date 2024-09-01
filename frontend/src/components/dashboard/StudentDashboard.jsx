c6a27336b3260939a48fb4fbb96835c30990d8b1
kiran <kiransahukar1999@gmail.com>
2024-09-01 16:20:53 +0100

    new again

--- frontend/src/components/dashboard/StudentDashboard.jsx
+++ frontend/src/components/dashboard/StudentDashboard.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Button, Offcanvas } from 'react-bootstrap';
-import Header from "./../header/Header";
+
 import CourseDetails from "../leftSection/Student/CourseDetails.jsx";
 import RightComponent from "./../rightSection/RightComponent";
-import SessionDetails from "./Student/SessionDetails.jsx";
 
+import { useSelector } from "react-redux";
+import SessionDetails from "./Student/SessionDetails.jsx";
 const StudentDashboard = () => {
-  const [currentPage, setCurrentPage] = useState("home");
+  
   const [isLeftVisible, setIsLeftVisible] = useState(false);
   const [selectedCourse, setSelectedCourse] = useState();
   const [isRightVisible, setIsRightVisible] = useState(false);
 
-  const userId = 1;
+  const userId = useSelector((state)=>state.user.value.userId);
 
   const toggleLeftComponent = () => {
     setIsLeftVisible(!isLeftVisible);
@@ -22,26 +23,44 @@ const StudentDashboard = () => {
   };
 
   return (
+    // <div>Hello here</div>
     <Container fluid>
-      <Header onNavigate={setCurrentPage} />
+       
+      
 
       <Row>
-        <Col md={3} className={`d-flex flex-column ${isLeftVisible ? 'd-block' : 'd-none'}`}>
-          <Button onClick={toggleLeftComponent} variant="info" className="mb-3">
-            {isLeftVisible ? 'Hide Course Details' : 'Show Course Details'}
-          </Button>
-          <CourseDetails
+        {/* <Col md={3} className={`d-flex flex-column ${isLeftVisible ? 'd-block' : 'd-none'}`}> */}
+          
+          {/* <CourseDetails
             isVisible={isLeftVisible}
             selectedDetails={setSelectedCourse}
             userId={userId}
-          />
+          /> */}
+
+<Col md={1}>
+      <Button onClick={toggleLeftComponent} variant="outline-dark" className="flex-left m-1 ml-0">
+            {isLeftVisible ? 'Hide Course Details' : 'Show Course Details'}
+      </Button>
         </Col>
+
+
+        {/* <Button onClick={toggleLeftComponent} variant="info" className="fixed-left m-3">
+            {isLeftVisible ? 'Hide Course Details' : 'Show Course Details'}
+          </Button> */}
+
 
         <Col md={9}>
           <div className="mainContent">
-            <StudentDashboard courseId={selectedCourse} />
+            <SessionDetails courseId={selectedCourse} />
           </div>
         </Col>
+
+        <Col md={1}>
+        <Button onClick={toggleRightComponent} variant="outline-dark" className="fixed-left m-3">
+        Toggle Messages
+      </Button>
+        </Col>
+       
       </Row>
 
       <Offcanvas show={isRightVisible} onHide={toggleRightComponent} placement="end">
@@ -52,10 +71,19 @@ const StudentDashboard = () => {
           <RightComponent />
         </Offcanvas.Body>
       </Offcanvas>
-
-      <Button onClick={toggleRightComponent} variant="info" className="fixed-bottom m-3">
-        Toggle Messages
-      </Button>
+      <Offcanvas show={isLeftVisible} onHide={toggleLeftComponent} placement="start">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Messages</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+        <CourseDetails
+            isVisible={isLeftVisible}
+            selectedDetails={setSelectedCourse}
+            userId={userId}
+          />
+        </Offcanvas.Body>
+      </Offcanvas>
+     
     </Container>
   );
 };
